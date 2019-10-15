e1b7fa6f36892032c0e1c9f25c72a415acc883a4
Annchouss <anyashi2002@gmail.com>
2019-10-15 14:41:57 +0300

    The problem with data fetching was solved. Response was recreated

--- app/src/main/java/com/example/forecastapplication/data/ApixuWeatherApiService.kt
+++ app/src/main/java/com/example/forecastapplication/data/ApixuWeatherApiService.kt
@@ -1,6 +1,6 @@
 package com.example.forecastapplication.data
 
-import com.example.forecastapplication.data.response.CurrentWeatherResponse
+import com.example.forecastapplication.data.response.CurrentWeatherResponce
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import kotlinx.coroutines.Deferred
 import okhttp3.Interceptor
@@ -12,48 +12,51 @@ import retrofit2.http.Query
 
 const val API_KEY = "fbfe0823665b8084369d11dab2e0ce9d"
 
-//http://api.weatherstack.com/current
-//    ? access_key = YOUR_ACCESS_KEY
-//    & query = New York
-
-//
 
 interface ApixuWeatherApiService {
 
-    @GET
+
+    // http://api.weatherstack.com/current?access_key=f2b8eb4d871f1e8eb89a27fd147a67cc&query=Sao%Paulo&lang=pt-br
+
+
+    @GET("current")
     fun getCurrentWeather(
-        @Query("query") location:String
-    ) : Deferred<CurrentWeatherResponse>
+        @Query("query") location: String = "london",
+        @Query("lang") languageCode: String = "en"
+    ): Deferred<CurrentWeatherResponce>
 
 
     companion object {
-        operator fun invoke(): ApixuWeatherApiService {
-           val requestInterceptor = Interceptor {chain ->
-               val url = chain.request()
-                   .url()
-                   .newBuilder()
-                   .addQueryParameter("access_key", API_KEY)
-                   .build()
+        operator fun invoke()
+                : ApixuWeatherApiService {
+            val requestInterceptor = Interceptor { chain ->
 
-               val request  = chain.request()
-                   .newBuilder()
-                   .url(url)
-                   .build()
+                val url = chain.request()
+                    .url()
+                    .newBuilder()
+                    .addQueryParameter("access_key", API_KEY)
+                    .build()
 
-               return@Interceptor chain.proceed(request)
-           }
+                val request = chain.request()
+                    .newBuilder()
+                    .url(url)
+                    .build()
+
+                return@Interceptor chain.proceed(request)
+            }
+
+
             val okHttpClient = OkHttpClient.Builder()
                 .addInterceptor(requestInterceptor)
                 .build()
 
             return Retrofit.Builder()
                 .client(okHttpClient)
-                .baseUrl("https://api.weatherstack.com/")
+                .baseUrl("http://api.weatherstack.com/")
                 .addCallAdapterFactory(CoroutineCallAdapterFactory())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
                 .create(ApixuWeatherApiService::class.java)
-
         }
     }
 }
--- app/src/main/java/com/example/forecastapplication/ui/weather/current/CurrentWeatherFragment.kt
+++ app/src/main/java/com/example/forecastapplication/ui/weather/current/CurrentWeatherFragment.kt
@@ -1,19 +1,17 @@
 package com.example.forecastapplication.ui.weather.current
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.example.forecastapplication.R
 import com.example.forecastapplication.data.ApixuWeatherApiService
 import kotlinx.android.synthetic.main.current_weather_fragment.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import okhttp3.Dispatcher
 
 class CurrentWeatherFragment : Fragment() {
 
@@ -31,15 +29,18 @@ class CurrentWeatherFragment : Fragment() {
         return inflater.inflate(R.layout.current_weather_fragment, container, false)
     }
 
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(CurrentWeatherViewModel::class.java)
         // TODO: Use the ViewModel
 
         val apiService = ApixuWeatherApiService()
-        GlobalScope.launch(Dispatchers.Main) {
-            val currentWeatherResponse = apiService.getCurrentWeather("London").await()
-            current_weather_text.text = currentWeatherResponse.currentWeatherEnrty.toString()
+
+
+        GlobalScope.launch(Dispatchers.Main){
+            val currentWeather = apiService.getCurrentWeather("London").await()
+            current_weather_text.text = currentWeather.current.toString()
         }
     }
 
